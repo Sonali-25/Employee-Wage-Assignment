ba2c48abe231a505397b2ac9beee967d9a85c76f
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-05-22 10:23:31 +0530

    Added Employee Wage Problem till uc-7

---
+++ EmpWageCalculator.java
@@ -0,0 +1,11 @@
+package com.empwage;
+
+public class EmpWageCalculator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To The Employee Wage Calculator");
+		uc7 uc=new uc7();
+		uc.computewage();
+	}
+
+}
---
+++ aaddress.java
@@ -0,0 +1,10 @@
+package com.empwage;
+
+public class aaddress {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
